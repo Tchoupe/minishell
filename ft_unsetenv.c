b03b3c58043a4e42abc20767a6501fb7b2cf4b4a
Tchoupe <ntom@student.s19.be>
2019-09-04 00:10:01 +0200

    unsetenv fonctionnel

--- ft_unsetenv.c
+++ ft_unsetenv.c
@@ -6,13 +6,53 @@
 /*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 19:54:44 by ntom              #+#    #+#             */
-/*   Updated: 2019/09/03 18:10:04 by ntom             ###   ########.fr       */
+/*   Updated: 2019/09/04 00:08:46 by ntom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-void	ft_unsetenv(t_info *infos)
+void			ft_unsetenv(t_info *infos)
 {
-	(void)infos;
+	int		i;
+	int		size;
+	int		j;
+	char	**tmp;
+	int		x;
+	int		y;
+
+	j = 1;
+	size = 0;
+	while (infos->envs[size])
+		size++;
+	if (infos->argc == 1)
+		return (ft_putendl("unsetenv: Too few arguments."));
+	while (infos->args[j])
+	{
+		if ((i = find_env(infos, infos->args[j]) != -1))
+		{
+			x = 0;
+			y = 0;
+			if (!(tmp = (char **)ft_memalloc(sizeof(char *) * (size + 1))))
+				return ;
+			while (infos->envs[x] && x < size)
+			{
+				if (ft_strcmp(infos->envs[x], infos->args[j]) == '=')
+				{
+					x++;
+					continue ;
+				}
+				if (!(tmp[y] = ft_strdup(infos->envs[x])))
+					return ;
+				x++;
+				y++;
+			}
+			tmp[size] = NULL;
+			size--;
+			ft_tabdel(infos->envs);
+			infos->envs = ft_tab_dup(tmp, -1);
+		}
+		j++;
+	}
+
 }
